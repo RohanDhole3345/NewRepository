51caea031b3bfcf646ead0304b987f15790dbe64
RohanDhole3345 <dholerohan18@gmail.com>
2025-12-12 16:43:42 +0530

    Task 04 added

---
+++ Task_04.py
@@ -0,0 +1,13 @@
+#Find the keyword on whole dataset
+import re
+file = r"C:\Users\ASUS\OneDrive\Desktop\Python 2025\Tasks\small.log"
+
+keyword = input("Enter a keyword to search you in File:").upper()
+count = 0
+
+with open(file, "r") as f:
+    for line in f:
+        if keyword in line.upper(): # check keyword in line
+            count += 1 #increase the ccount
+
+print(f"\nThe word'{keyword}'occurred {count} times in File.")
